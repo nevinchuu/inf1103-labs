709dba7e830b258b24b9d1bfc64247b543535762
nevinchua <2603219@sit.singaporetech.edu.sg>
2026-09-10 17:42:04 +0800

    Fifth part, Enforced Business rules

--- auditor.py
+++ auditor.py
@@ -2,7 +2,7 @@ inventory = 0
 
 quantity = input("Enter Stock Quantity or type 'quit' to exit: ")
 while quantity != "quit":
-    if not quantity.isdigit():
+    if not quantity.isdigit() or int(quantity) < 0:
         print("Invalid input. Please enter a valid number or type 'quit' to exit.")
     else: 
     #Accidentally added int alr so this will be the third commit
