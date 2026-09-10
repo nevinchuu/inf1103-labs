f02f98396ebe47d991c319e3ef5733117cfd5ade
nevinchua <2603219@sit.singaporetech.edu.sg>
2026-09-10 17:39:56 +0800

    Forth Part

--- auditor.py
+++ auditor.py
@@ -2,8 +2,12 @@ inventory = 0
 
 quantity = input("Enter Stock Quantity or type 'quit' to exit: ")
 while quantity != "quit":
+    if not quantity.isdigit():
+        print("Invalid input. Please enter a valid number or type 'quit' to exit.")
+    else: 
     #Accidentally added int alr so this will be the third commit
-    inventory += int(quantity)
+        inventory += int(quantity)
+    
     quantity = input("Enter Stock Quantity or type 'quit' to exit: ")
 
 print("Total Inventory: " + str(inventory))
