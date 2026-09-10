8a856f153193d530cb0e7996ee5a522772779da7
nevinchua <2603219@sit.singaporetech.edu.sg>
2026-09-10 17:37:29 +0800

    Third Part

--- auditor.py
+++ auditor.py
@@ -2,6 +2,7 @@ inventory = 0
 
 quantity = input("Enter Stock Quantity or type 'quit' to exit: ")
 while quantity != "quit":
+    #Accidentally added int alr so this will be the third commit
     inventory += int(quantity)
     quantity = input("Enter Stock Quantity or type 'quit' to exit: ")
 
