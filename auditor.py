3efedf1cbb21a5d53d372737ed595a99a00fd1a9
nevinchua <2603219@sit.singaporetech.edu.sg>
2026-09-10 17:45:38 +0800

    Part Seven - Trigger Overstock Alert

--- auditor.py
+++ auditor.py
@@ -2,13 +2,15 @@ inventory = 0
 
 quantity = input("Enter Stock Quantity or type 'quit' to exit: ")
 while quantity != "quit":
-    if not quantity.isdigit() or int(quantity) < 0:
+    if not quantity.isdigit() or int(quantity) <= 0:
         print("Invalid input. Please enter a valid number or type 'quit' to exit.")
-    else: 
-    #Accidentally added int alr so this will be the third commit
+    elif int(inventory) + int(quantity) > 500:
+            print("Inventory limit exceeded. Current Inventory: ", inventory)
+            break
+    else:
         inventory += int(quantity)
-        print("Current Inventory: " + str(inventory))
-    
+        print("Current Inventory: ", inventory)
+
     quantity = input("Enter Stock Quantity or type 'quit' to exit: ")
 
 print("Total Inventory: " + str(inventory))
