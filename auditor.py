e05a4e3a398147b28fe52eab672922a9b61ab439
nevinchua <2603219@sit.singaporetech.edu.sg>
2026-09-10 17:34:59 +0800

    Second Part

--- auditor.py
+++ auditor.py
@@ -1,1 +1,8 @@
 inventory = 0
+
+quantity = input("Enter Stock Quantity or type 'quit' to exit: ")
+while quantity != "quit":
+    inventory += int(quantity)
+    quantity = input("Enter Stock Quantity or type 'quit' to exit: ")
+
+print("Total Inventory: " + str(inventory))
