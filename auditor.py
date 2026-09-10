1cb1daee9f4f4dcf5463df82a13f3f85f27ba4fb
nevinchua <2603219@sit.singaporetech.edu.sg>
2026-09-10 17:46:39 +0800

    Part Eight - Reporting

--- auditor.py
+++ auditor.py
@@ -1,11 +1,14 @@
 inventory = 0
+rejected_entries = 0
 
 quantity = input("Enter Stock Quantity or type 'quit' to exit: ")
 while quantity != "quit":
     if not quantity.isdigit() or int(quantity) <= 0:
         print("Invalid input. Please enter a valid number or type 'quit' to exit.")
+        rejected_entries += 1
     elif int(inventory) + int(quantity) > 500:
             print("Inventory limit exceeded. Current Inventory: ", inventory)
+            rejected_entries += 1
             break
     else:
         inventory += int(quantity)
@@ -13,4 +16,5 @@ while quantity != "quit":
 
     quantity = input("Enter Stock Quantity or type 'quit' to exit: ")
 
-print("Total Inventory: " + str(inventory))
+print("Final Inventory: ", inventory)
+print("Rejected Entries: ", rejected_entries)
